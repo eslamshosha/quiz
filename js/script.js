440e6bec7e8a2ac9d4df0dcd493ce12a1e9ed4c2
eslam shosha <you@example.comeslamshosha22.es@gmail.com>
2024-06-06 04:36:55 +0300

    new

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ window.onload = function () {
 $(document).ready(function () {
   ///////// ** related** /////////
   var specials = new Swiper(".related-slider .swiper-container", {
-    loop: true,
+    // loop: true,
     autoplay: true,
     slidesPerView: 3.2,
     spaceBetween: 20,
@@ -32,39 +32,3 @@ $(document).ready(function () {
     },
   });
 });
-const timerExists =
-  document.getElementsByClassName("countdown-container").length > 0;
-if (timerExists) {
-  //timer setting
-  const daysEl = document.getElementById("days");
-  const hoursEl = document.getElementById("hours");
-  const minsEl = document.getElementById("mins");
-  const secondsEl = document.getElementById("seconds");
-
-  const newYears = "31 Dec 2024";
-
-  function countdown() {
-    const newYearsDate = new Date(newYears);
-    const currentDate = new Date();
-
-    const totalSeconds = (newYearsDate - currentDate) / 1000;
-
-    const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const mins = Math.floor(totalSeconds / 60) % 60;
-    const seconds = Math.floor(totalSeconds) % 60;
-
-    daysEl.innerHTML = days;
-    hoursEl.innerHTML = formatTime(hours);
-    minsEl.innerHTML = formatTime(mins);
-    secondsEl.innerHTML = formatTime(seconds);
-  }
-
-  function formatTime(time) {
-    return time < 10 ? `0${time}` : time;
-  }
-
-  // initial call
-  countdown();
-  setInterval(countdown, 1000);
-}
